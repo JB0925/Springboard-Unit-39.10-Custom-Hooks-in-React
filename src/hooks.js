c3892ae8e7da38bebb5cf85125d3c8a99c7141ba
Jesse Brink <jbrink0925@gmail.com>
2021-10-17 14:11:30 -0400

    add functionality to make AJAX requests and change url dependent upon if a name gets passed in or not

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { v4 as uuid } from "uuid";
+import pokemon from "./pokemonList";
 
 const useFlip = (defaulValue = true) => {
     const [value, setValue] = useState(defaulValue);
@@ -8,4 +11,18 @@ const useFlip = (defaulValue = true) => {
     return [value, flipCard];
 };
 
-export default useFlip;
+const useAxios = url => {
+    const [cards, setCards] = useState([]);
+    const addCard = async(name) => {
+        url = pokemon.indexOf(name) !== -1 ? `${url}${name}/` : url;
+        const response = await axios.get(url);
+        setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+    }
+    return [cards, addCard];
+};
+
+
+export {
+    useFlip,
+    useAxios
+};
